ef347a11f86bbcca38d3bc4065b554aedabffd50
MotazAboAssi <motazaboassi@gmail.com>
2026-04-13 01:08:06 +0300

    add security new layer  used bcryptjs  by hash password. and remove access to password until if it was hash

--- router/user.route.js
+++ router/user.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require('bcryptjs');
 const { User } = require('./../models/user.model');
 
 const userRouter = express.Router();
@@ -6,18 +7,24 @@ const userRouter = express.Router();
 // GET 
 userRouter.get('/', async (req, res) => {
     const users = await User.find({});
-    res.status(200).json(users)
+    const secureUsers = users.map((user) => {
+        const { __v, password, ...secureUser } = user._doc;
+        return secureUser;
+    })
+    res.status(200).json(secureUsers)
 });
 // POST
 userRouter.post('/', async (req, res) => {
     let user = await req.body;
     if (user !== undefined) {
         try {
+            user['password'] = bcrypt.hashSync(user['password'], 10);
             const newUser = new User(user);
             await newUser.save();
+            const { password, ...secureUser } = user;
             return res.status(200).json({
                 status: 'Success',
-                user
+                user: secureUser
             })
         } catch (error) {
             return res.status(400).json({
@@ -39,7 +46,11 @@ userRouter.get('/:id', async (req, res) => {
     const _id = req.params.id
     if (_id !== undefined) {
         const user = await User.find({ _id })
-        res.status(200).json({ status: 'Success', user })
+        if (user.length !== 0) {
+            const { password, __v, ...secureUser } = user['0']._doc
+            res.status(200).json({ status: 'Success', user: secureUser })
+        } else
+            res.status(400).json({ status: 'Faild', message: 'user not found' })
     } else {
         res.status(400).json({ status: 'Faild', message: error.message })
     }
@@ -50,7 +61,8 @@ userRouter.put('/:id', async (req, res) => {
     const userInfo = req.body;
     if (_id !== undefined) {
         const user = await User.findByIdAndUpdate(_id, { $set: userInfo }, { new: true })
-        res.status(200).json({ status: 'Success', user })
+        const { password, __v, ...secureUser } = user._doc
+        res.status(200).json({ status: 'Success', secureUser })
     } else {
         res.status(400).json({ status: 'Faild', message: error.message })
     }
@@ -60,8 +72,10 @@ userRouter.delete('/:id', async (req, res) => {
     const _id = req.params.id
     if (_id !== undefined) {
         const user = await User.findByIdAndDelete(_id);
-        if (user !== null)
-            res.status(200).json({ status: 'Success', user })
+        if (user !== null) {
+            const { password, __v, ...secureUser } = user._doc
+            res.status(200).json({ status: 'Success', secureUser })
+        }
         else
             res.status(400).json({ status: 'Faild', message: 'User not found' })
     } else {
