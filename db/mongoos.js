a0e7904f5cbd285324c501791832f614107dac48
MotazAboAssi <motazaboassi@gmail.com>
2026-04-15 12:33:39 +0300

    handle case disconnect DB

--- db/mongoos.js
+++ db/mongoos.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 async function connectionDB() {
     try {
-        const connect = await mongoose.connect('mongodb://localhost:27017/express-user-crud-api');
+        const connect = await mongoose.connect('mongodb://localhost:27017/express-user-crud-api', { serverSelectionTimeoutMS: 2000 });
         console.log('✅ Connection MongoDB');
     } catch (error) {
         console.log('❌ Connection MongoDB');
+        throw new Error('❌ Connection MongoDB');
     }
+    return;
 }
 
 module.exports = { connectionDB };
